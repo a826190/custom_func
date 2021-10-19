9a6283cd28a35789fd5a440a747a61b2acc4ea07
a826190 <91068205+a826190@users.noreply.github.com>
2021-10-19 12:14:00 +0530

    made function my own

---
+++ custom/multiplybyfactor_aguptav1.py
@@ -0,0 +1,44 @@
+import logging
+
+from iotfunctions import ui
+from iotfunctions.base import BaseTransformer
+
+logger = logging.getLogger(__name__)
+
+# Specify the URL to your package here.
+# This URL must be accessible via pip install
+
+PACKAGE_URL = 'git+https://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@github.com/a826190/custom_func.git@starter_package'
+
+
+class MultiplyByFactor_aguptav1(BaseTransformer):
+
+    def __init__(self, input_items, factor, output_items):
+
+        self.input_items = input_items
+        self.output_items = output_items
+        self.factor = float(factor)
+        super().__init__()
+    def execute(self, df):
+        df = df.copy()
+        for i,input_item in enumerate(self.input_items):
+            df[self.output_items[i]] = df[input_item] * self.factor
+        return df
+
+    @classmethod
+    def build_ui(cls):
+        #define arguments that behave as function inputs
+        inputs = []
+        inputs.append(ui.UIMultiItem(
+                name = 'input_items',
+                datatype=float,
+                description = "Data items adjust",
+                output_item = 'output_items',
+                is_output_datatype_derived = True)
+                      )
+        inputs.append(ui.UISingle(
+                name = 'factor',
+                datatype=float)
+                      )
+        outputs = []
+        return (inputs,outputs)
